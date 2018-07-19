c73482701b9a5c20e2efef79253b4cc753c66009
Takafumi Arakaki <aka.tkf@gmail.com>
2018-07-19 16:42:34 -0700

    Add SIMD tests for vectorized power

--- test/SIMDTest.jl
+++ test/SIMDTest.jl
@@ -3,6 +3,7 @@ module SIMDTest
 using Compat
 using Compat.Test
 using ForwardDiff: Dual, valtype
+using StaticArrays: SVector
 
 const DUALS = (Dual(1., 2., 3., 4.),
                Dual(1., 2., 3., 4., 5.),
@@ -16,6 +17,14 @@ function simd_sum(x::Vector{T}) where T
     end
     return s
 end
+
+# `pow2dot` is chosen so that `@code_llvm pow2dot(SVector(1:1.0:4...))`
+# generates code with SIMD instructions.
+# See:
+# https://github.com/JuliaDiff/ForwardDiff.jl/pull/332
+# https://github.com/JuliaDiff/ForwardDiff.jl/pull/331#issuecomment-406107260
+@inline pow2(x) = x^2
+pow2dot(xs) = pow2.(xs)
 
 for D in map(typeof, DUALS)
     plus_bitcode = sprint(io -> code_llvm(io, +, (D, D)))
@@ -42,6 +51,9 @@ for D in map(typeof, DUALS)
         sum_bitcode = sprint(io -> code_llvm(io, simd_sum, (Vector{D},)))
         @test ismatch(r"fadd \<.*?x double\>", sum_bitcode)
     end
+
+    pow_bitcode = sprint(io -> code_llvm(io, pow2dot, (SVector{4, D},)))
+    @test ismatch(r"(.*fmul \<.*?x double\>){2}"s, pow_bitcode)
 end
 
 end # module
